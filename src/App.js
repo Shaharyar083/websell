6f9ab96e232b7361daa1382c7e78437c9940df2a
shaharyar083 <shaharyarilyas7@gmail.com>
2022-01-05 21:59:21 +0500

    05/01/2022

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Buy from "./components/Buy/Buy";
 import ContactUs from "./components/Contact Us/ContactUs";
 import Home from "./components/Home Page/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Sell from "./components/Sell/Sell";
 import WebsiteValuation from "./components/Website Valuation/WebsiteValuation";
 
 function App() {
@@ -13,6 +16,10 @@ function App() {
         <Route path="/website-valuation-tool" element={<WebsiteValuation />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/buy" element={<Buy />} />
+        <Route path="/sell" element={<Sell />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
---
+++ src/components/Sell/Sell.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import "./sellPage.scss";
+import SellSection1 from "./Sell Section/SellSection1";
+
+const Sell = () => {
+  return (
+    <>
+      <Navbar />
+      <SellSection1 />
+      <Footer />
+    </>
+  );
+};
+
+export default Sell;
