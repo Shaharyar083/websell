147de3043972efb71c7d22404854fe15af70c11a
Junaid Latif <junaidlatif004@gmail.com>
2022-01-05 23:18:43 +0500

    Post Route Added

---
+++ BackEnd/utility/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2
+
+cloudinary.config({
+    cloud_name: "dg0naizkn",
+    api_key: "931256592781414",
+    api_secret: "SzS4kHA_ro1U7nK67uhc7HNgdvg"
+})
+
+module.exports = cloudinary
